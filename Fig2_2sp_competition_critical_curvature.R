bd2bd185b554b09ead48c14e077140ba01bfe0d0
nick-lowery <40038363+nick-lowery@users.noreply.github.com>
2018-09-12 16:19:52 -0700

    Update header and clarify commenting

--- Fig2_2sp_competition_critical_curvature.R
+++ Fig2_2sp_competition_critical_curvature.R
@@ -1,4 +1,19 @@
-# 2sp asymmetric competition - calculation of critical curvature
+###############################################################################
+### Figure 2B - Critical curvature of competition interfaces ##################
+###############################################################################
+
+# Nick Lowery and Tristan Ursell
+# 2018
+# 
+# This script generates Figure 2B from our preprint:
+# Structured environments fundamentally alter dynamics and stability of ecological communities
+# https://www.biorxiv.org/content/early/2018/07/10/366559
+# 
+# The script reads in metadata files generated from the LV_competition_two_species_asym_curvature.m
+# function. The functions to generate the cleaned data from the raw .mat data 
+# files are included but commented out.
+
+###############################################################################
 
 #library(R.matlab)
 library(tidyverse)
@@ -26,10 +41,34 @@ library(colorspace)
 # write_csv(curve.data, "E:/ecological_modelling/2sp_critical_curvature.csv")
 curve.data <- read_csv("E:/ecological_modelling/2sp_critical_curvature.csv")
 
-# visualise
-pal <- choose_palette()
+# define color palette (generated from colorspace GUI)
+pal <- function (n, h = c(300, 75), c. = c(35, 95), l = c(15, 90), power = c(0.8, 
+                 1.2), fixup = TRUE, gamma = NULL, alpha = 1, ...) 
+  {
+    if (!is.null(gamma)) 
+        warning("'gamma' is deprecated and has no effect")
+    if (n < 1L) 
+        return(character(0L))
+    h <- rep(h, length.out = 2L)
+    c <- rep(c., length.out = 2L)
+    l <- rep(l, length.out = 2L)
+    power <- rep(power, length.out = 2L)
+    rval <- seq(1, 0, length = n)
+    rval <- hex(polarLUV(L = l[2L] - diff(l) * rval^power[2L], 
+        C = c[2L] - diff(c) * rval^power[1L], H = h[2L] - diff(h) * 
+            rval), fixup = fixup, ...)
+    if (!missing(alpha)) {
+        alpha <- pmax(pmin(alpha, 1), 0)
+        alpha <- format(as.hexmode(round(alpha * 255 + 1e-04)), 
+            width = 2L, upper.case = TRUE)
+        rval <- paste(rval, alpha, sep = "")
+    }
+    return(rval)
+  }
 
+# create plot
 curve.data %>%
+  # filter out data for which curve estimation is unreliable (due to small finite number of pixels)
   filter(gap >= 30) %>%
   ggplot(aes(x = delta, y = K, color = gap, group = gap)) +
   geom_point(size = 3) + 
